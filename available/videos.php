f95f243cc0fb1d13944f8f6f0336faa3110d0a9f
webuser <ryan@virtuecenter.com>
2013-10-09 10:50:58 -0400

    make videos determine id from url in feeds

--- available/videos.php
+++ available/videos.php
@@ -7,7 +7,11 @@ class videos {
 	public $publishable = true;
 	public static $singular = 'video';
 
-	public function document () {
-		
+	public function document (&$document) {
+		$document['video_id'] = null;
+		$document['video_type'] = null;
+		if (!empty($document['video'])) {
+			$document['video_id'] = UrlId::parse($document['video'], $document['video_type']);
+		}
 	}
 }

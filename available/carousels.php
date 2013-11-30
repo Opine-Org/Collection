9b75f1b2074c1ffd8df88030d96c97ceb17867ad
webuser <ryan@virtuecenter.com>
2013-11-30 11:15:29 -0500

    carousel collection

---
+++ available/carousels.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * @version .2
+ * @link https://raw.github.com/virtuecenter/collection/master/available/carousels.php
+ * @mode upgrade
+ */
+namespace Collection;
+
+class carousels {
+	public $publishable = false;
+	public $singular = 'carousel';
+	public $permalink = '/carousel/';
+
+	public function index ($document) {
+		$depth = substr_count($document['dbURI'], ':');
+		if ($depth > 1) {
+			return false;
+		}
+		return [
+			'title' => $document['title'], 
+			'description' => $document['description'], 
+			'image' => '',
+			'tags' => isset($document['tags']) ? $document['tags'] : [], 
+			'categories' => [],
+			'date' => date('c', $document['created_date']->sec) 
+		];
+	}
+}

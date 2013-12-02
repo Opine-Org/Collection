af4fba95b03fc7aa1d038d5d899274852386adb4
webuser <ryan@virtuecenter.com>
2013-12-02 11:22:01 -0500

    remove dead code

--- available/photo_galleries.php
+++ available/photo_galleries.php
@@ -1,15 +1,16 @@
 <?php
 /*
- * @version .3
+ * @version .4
  * @link https://raw.github.com/virtuecenter/collection/master/available/photo_galleries.php
  * @mode upgrade
+ *
+ * .4 remove dead code
  */
 namespace Collection;
 
 class photo_galleries {
 	public $publishable = true;
 	public $singular = 'photo_gallery';
-	public $permalink = '/photo_gallery/';
 
 	public function index ($document) {
 		$depth = substr_count($document['dbURI'], ':');

f2a3f2efac3b660e43ebce52d782ec79e97de0de
webuser <ryan@virtuecenter.com>
2013-12-07 22:37:39 -0500

    =collection updates

--- available/blurbs_by_tag.php
+++ available/blurbs_by_tag.php
@@ -1,12 +1,13 @@
 <?php
 /*
- * @version .4
+ * @version .5
  * @link https://raw.github.com/virtuecenter/collection/master/available/blurbs_by_tag.php
  * @mode upgrade
  *
  * .2 reshape output
  * .3 syntax
  * .4 typo
+ * .5 infinite loop
  */
 namespace Collection;
 
@@ -16,7 +17,7 @@ class blurbs_by_tag {
 	public $path = false;
 
 	public function all ($collection) {
-		$documents = $collection->all();
+		$documents = $collection->fetch();
 		$newDocs = [];
 		
 		foreach ($documents as $document) {

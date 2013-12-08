acbe49a6f63ee1d11445c4a71f6d874bb2ceb164
webuser <ryan@virtuecenter.com>
2013-12-07 22:30:08 -0500

    =collection updates

--- available/blurbs_by_tag.php
+++ available/blurbs_by_tag.php
@@ -1,10 +1,11 @@
 <?php
 /*
- * @version .2
+ * @version .3
  * @link https://raw.github.com/virtuecenter/collection/master/available/blurbs_by_tag.php
  * @mode upgrade
  *
  * .2 reshape output
+ * .3 syntax
  */
 namespace Collection;
 
@@ -16,8 +17,9 @@ class blurbs_by_tag {
 	public function all ($collection) {
 		$documents = $collectiton->all();
 		$newDocs = [];
+		
 		foreach ($documents as $document) {
-			$newDocs[$document['_id']] = >$document['value'];
+			$newDocs[$document['_id']] = $document['value'];
 		}
 
 		return $newDocs;

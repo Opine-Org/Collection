c9db51f7a697a851e476abf7d2c0f017102dcea4
Zeeshan Sattar <zeeshan@virtuecenter.com>
2013-12-15 11:01:21 -0500

    jobs collection added

--- available/jobs.php
+++ available/jobs.php
@@ -1,12 +1,23 @@
 <?php
 /*
- * @version .2
+ * @version .3
  * @link https://raw.github.com/virtuecenter/collection/master/available/jobs.php
  * @mode upgrade
  */
 namespace Collection;
 
 class jobs {
-	public $publishable = true;
+	public $publishable = false;
 	public $singular = 'job';
+
+	public function index ($document) {
+		return [
+			'title' => $document['title'], 
+			'description' => $document['body'], 
+			'image' => null, 
+			'tags' => isset($document['tags']) ? $document['tags'] : [], 
+			'categories' => [],
+			'date' => date('c', $document['created_date']->sec) 
+		];
+	}
 }

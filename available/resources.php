7eaa91be4cc8ff199ef9256e8122cf074cd4b606
Zeeshan Sattar <zeeshan@virtuecenter.com>
2013-12-23 11:06:12 -0500

    resources updated

--- available/resources.php
+++ available/resources.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * @version .2
+ * @version .3
  * @link https://raw.github.com/virtuecenter/collection/master/available/resources.php
  * @mode upgrade
  */
@@ -9,4 +9,18 @@ namespace Collection;
 class resources {
 	public $publishable = true;
 	public $singular = 'resource';
+	public function index ($document) {
+		return [
+			'title' => $document['title'],
+			'description' => [''],
+			'image' => isset($document['image']) ? $document['image'] : '', 
+			'tags' => isset($document['tags']) ? $document['tags'] : [], 
+			'categories' => isset($document['categories']) ? $document['categories']: [],
+			'date' => date('c', $document['created_date']->sec) 
+		];
+	}
+
+	public function tagsView ($mode, $id, $document) {
+		$this->queue->add('CollectionTags', ['collection' => 'resources']);
+	}
 }

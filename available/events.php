135850d57eb8c05a3ee620da2ffa46abe3f3329d
webuser <ryan@virtuecenter.com>
2014-01-09 00:32:12 -0500

    index enhancement

--- available/events.php
+++ available/events.php
@@ -1,10 +1,11 @@
 <?php
 /*
- * @version .3
+ * @version .4
  * @link https://raw.github.com/virtuecenter/collection/master/available/events.php
  * @mode upgrade
  *
  * .3 tag view
+ * .4 index enhancement
  */
 namespace Collection;
 
@@ -13,6 +14,10 @@ class events {
 	public $singular = 'event';
 
 	public function index ($document) {
+		$depth = substr_count($document['dbURI'], ':');
+		if ($depth > 1) {
+			return false;
+		}
 		return [
 			'title' => $document['title'], 
 			'description' => $document['description'], 

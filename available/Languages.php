6c4a85c094ab8af421cb25ae1006d960cc47dcb4
webuser <ryan@virtuecenter.com>
2014-11-02 11:52:30 -0500

    new collections for language and user groups

---
+++ available/Languages.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @version .1
+ * @link https://raw.github.com/Opine-Org/Collection/master/available/Languages.php
+ * @mode upgrade
+ */
+namespace Collection;
+
+class Languages {
+    public $publishable = false;
+    public $singular = 'language';
+}
---
+++ available/UserGroups.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * @version .1
+ * @link https://raw.github.com/Opine-Org/Collection/master/available/Languages.php
+ * @mode upgrade
+ */
+namespace Collection;
+
+class UserGroups {
+    public $publishable = false;
+    public $singular = 'user_group';
+}
